5b1ccf86e1d3dbb59b35436c46ff0f636ef3634e
szn <80252484@qq.com>
2023-07-31 18:52:13 +0800

    Add a license to each file.

--- grid_layout/src/define.h
+++ grid_layout/src/define.h
@@ -1,4 +1,28 @@
-﻿#ifndef VGG_GRID_DEFINE
+﻿/*
+MIT License
+
+Copyright (c) 2023 Very Good Graphics
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef VGG_GRID_DEFINE
 #define VGG_GRID_DEFINE
 
 #ifndef NDEBUG
@@ -20,12 +44,6 @@ enum grid_auto_flow
     grid_auto_flow_row = 1,
     grid_auto_flow_row_dense = 2
 };
-
-// enum expand_strategy
-// {
-//     expand_strategy_auto = 1,
-//     expand_strategy_fix_column = 2
-// };
 
 enum column_width_strategy
 {
