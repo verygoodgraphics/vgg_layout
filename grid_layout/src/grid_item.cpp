74ccb7fd2d22b6f086851f1133275911a754c262
szn <80252484@qq.com>
2023-09-14 17:12:43 +0800

    1.changed abs to std::abs. 2.changed the way to download googletest.

--- grid_layout/src/grid_item.cpp
+++ grid_layout/src/grid_item.cpp
@@ -140,10 +140,10 @@ bool area_conflict(const grid_item &item1, const grid_item &item2)
     long long column_start_2 = item2.get_column_id();
     long long column_end_2 = column_start_2 + item2.get_column_span();    
 
-    double center_row = abs((row_start_1 + row_end_1) / 2.0 - (row_start_2 + row_end_2) / 2.0);
+    double center_row = std::abs((row_start_1 + row_end_1) / 2.0 - (row_start_2 + row_end_2) / 2.0);
     double half_sum_row = (item1.get_row_span() + item2.get_row_span()) / 2.0;
 
-    double center_column = abs((column_start_1 + column_end_1) / 2.0 - (column_start_2 + column_end_2) / 2.0);
+    double center_column = std::abs((column_start_1 + column_end_1) / 2.0 - (column_start_2 + column_end_2) / 2.0);
     double half_sum_column = (item1.get_column_span() + item2.get_column_span()) / 2.0;
 
     if (center_row < half_sum_row && center_column < half_sum_column)
